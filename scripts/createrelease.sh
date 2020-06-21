fa3c305890c4f334d53c41295759ae2a39b5ff0c
rickschubert <rick@tray.io>
2020-06-21 13:40:13 +0100

    updated release script

--- scripts/createrelease.sh
+++ scripts/createrelease.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-TAG_VERSION=$(git describe --tags)
-GOOS=darwin go build -o build/jira-ticket-mac_$TAG_VERSION
-GOOS=linux go build -o build/jira-ticket-linux_$TAG_VERSION
+LATEST_VERSION=$(git tag -l | tail -1)
+LATEST_MINOR=$(echo $LATEST_VERSION | sed 's/v1.0.//')
+NEW_MINOR="$(($LATEST_MINOR + 1))"
+NEW_VERSION=v1.0.$NEW_MINOR
+echo $NEW_VERSION
+
+git tag $NEW_VERSION
+git push origin tags/$NEW_VERSION
+
+GOOS=darwin go build -o build/jira-ticket-mac_$NEW_VERSION
+GOOS=linux go build -o build/jira-ticket-linux_$NEW_VERSION
